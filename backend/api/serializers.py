3426b1efcddf46db599e13efc041acd88ac0a327
Mohamed Abohend <mhmdabohend@gmail.com>
2025-11-10 18:55:16 +0200

    first commit

---
+++ backend/api/serializers.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from .models import Note
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "username", "password"]
+        extra_kwargs = {"password": {"write_only": True}}
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
+    
+class NoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Note
+        fields = ["title", "content","created_at", "author"]
+        extra_kwargs = {"author": {"write_only": True}}
---
+++ backend/api/views.py
@@ -0,0 +1,27 @@
+from rest_framework import generics
+from api.serializers import UserSerializer, NoteSerializer
+from django.contrib.auth.models import User
+from .models import Note
+from rest_framework.permissions import AllowAny, IsAuthenticated
+
+class CreateUserView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
+
+class NoteListCreate(generics.CreateAPIView):
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]
+    
+    def get_queryset(self):
+        return Note.objects.filter(author=self.request.user)
+    
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+class NoteDelete(generics.DestroyAPIView):
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Note.objects.filter(author=self.request.user)
